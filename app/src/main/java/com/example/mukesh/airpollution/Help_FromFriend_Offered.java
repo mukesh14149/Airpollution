5ddcce6e62477ceb98f95e7d2e7226599f33964b
mukesh <mukesh14149@iiitd.ac.in>
2016-04-30 02:58:44 +0530

    updated

--- app/src/main/java/com/example/mukesh/airpollution/Help_FromFriend_Offered.java
+++ app/src/main/java/com/example/mukesh/airpollution/Help_FromFriend_Offered.java
@@ -1,11 +1,16 @@
 package com.example.mukesh.airpollution;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class Help_FromFriend_Offered extends AppCompatActivity {
 
@@ -26,5 +31,39 @@ public class Help_FromFriend_Offered extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingActivity.class));
+            return true;
+        }
+
+
+        if(id==R.id.action_about){
+            Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.about_message), Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 10);
+            toast.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
